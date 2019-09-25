b925d91512fb6db8826d0cd67d4f1b2325850deb
mshimada <mshimada2@mail.csuchico.edu>
2019-09-25 11:19:44 -0700

    This commit fixes both #4 and #5 issues

--- main.cpp
+++ main.cpp
@@ -13,7 +13,10 @@ int main()
   char decision1 = ' ';
   char decision2 = ' ';
 
+  int notPlaying = 1;
 
+do
+{
   cout<<"Player 1, please enter your name: ";
   cin>>player1name;
   cout<<"Player 2, please enter your name: ";
@@ -33,17 +36,17 @@ int main()
   cout<<player2name<<", please enter the character of your choice...\n";
   cin>>decision2;
 
-  if(decision1 == 't' && decision2 == 's')
+  if(decision1 == 't' && decision2 == 's' && decision1 =='T' && decision2 =='S')
   {
     cout<<player1name<<" stole while "<<player2name<<" split. "
         <<player1name<<" WINS ALL OF THE PRIZE!\n";
   }
-  else if(decision2 == 't' && decision1 == 's')
+  else if(decision2 == 't' && decision1 == 's' && decision2 == 'T' && decision1 == 'S')
   {
     cout<<player2name<<" stole while "<<player1name<<" split. "
         <<player2name<<" WINS ALL OF THE PRIZE!\n";
   }
-  else if(decision1 == 's' && decision2 == 's')
+  else if(decision1 == 's' && decision2 == 's' && decision1 =='S' && decision2 == 'S') 
   {
     cout<<"Both players cooperated and chose to split. Congrats, "
         <<player1name<<" and "<<player2name<<" you each get half the prize!\n";
@@ -54,6 +57,18 @@ int main()
         <<"win a prize and go home with NOTHING!\n";
   }
 
+  cout<<"Do you want to play again?"<<endl;
+  cout<<"If not press 1 and if you do want to play again enter any number"<<endl;
+  cin>>notPlaying;
+
+  if(notPlaying == 1)
+  {
+    return 0;
+  }
+
+
+
+}while(notPlaying != 1);
 
 
 
