56fc8c4b2926e028de2af00c7ff25c8025b85a6a
kippyan <kippyan@gmail.com>
2019-09-25 11:18:13 -0700

    added option for choosing to play again

--- main.cpp
+++ main.cpp
@@ -14,47 +14,58 @@ int main()
   char decision1 = ' ';
   char decision2 = ' ';
 
+  string keepPlaying = "Y";
 
   cout<<"Player 1, please enter your name: ";
   cin>>player1name;
   cout<<"Player 2, please enter your name: ";
   cin>>player2name;
 
-  cout<<"WELCOME TO SPLIT OR STEAL!\n";
+  while(tolower(keepPlaying) == "y"){
 
-  cout<<player1name<<", please enter the character of your choice...\n";
-  cout<<"Please enter (s)plit or s(t)eal: ";
-  cin>>decision1;
+    cout<<"WELCOME TO SPLIT OR STEAL!\n";
 
-  cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    cout<<player1name<<", please enter the character of your choice...\n";
+    cout<<"Please enter (s)plit or s(t)eal: ";
+    cin>>decision1;
+
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
       <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
       <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
       <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
-  cout<<player2name<<", please enter the character of your choice...\n";
-  cin>>decision2;
+    cout<<player2name<<", please enter the character of your choice...\n";
+    cin>>decision2;
 
-  if(tolower(decision1) == 't' && tolower(decision2) == 's')
-  {
-    cout<<player1name<<" stole while "<<player2name<<" split. "
+    if(tolower(decision1) == 't' && tolower(decision2) == 's')
+    {
+      cout<<player1name<<" stole while "<<player2name<<" split. "
         <<player1name<<" WINS ALL OF THE PRIZE!\n";
-  }
-  else if(tolower(decision2) == 't' && tolower(decision1) == 's')
-  {
-    cout<<player2name<<" stole while "<<player1name<<" split. "
+    }
+    else if(tolower(decision2) == 't' && tolower(decision1) == 's')
+    {
+      cout<<player2name<<" stole while "<<player1name<<" split. "
         <<player2name<<" WINS ALL OF THE PRIZE!\n";
-  }
-  else if(tolower(decision1) == 's' && tolower(decision2) == 's')
-  {
-    cout<<"Both players cooperated and chose to split. Congrats, "
+    }
+    else if(tolower(decision1) == 's' && tolower(decision2) == 's')
+    {
+      cout<<"Both players cooperated and chose to split. Congrats, "
         <<player1name<<" and "<<player2name<<" you each get half the prize!\n";
-  }
-  else
-  {
-    cout<<"Sorry, you were both greedy and chose to steal. Neither of you "
+    }
+    else
+    {
+      cout<<"Sorry, you were both greedy and chose to steal. Neither of you "
         <<"win a prize and go home with NOTHING!\n";
-  }
+    }
+    cout<< "Enter Y to play again or N to quit.\n";
+    cin >> keepPlaying; 
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+      <<"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
+
+  }
 
 
 
